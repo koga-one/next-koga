45b2d62869af82cce9ef491eb8327fbd51d2257d
André Koga <social@koga.one>
2022-06-06 16:50:25 -0300

    added routing back in lol

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import { getPosts, getPostDetails } from "../../services";
 import {
   PostDetail,
@@ -8,8 +8,11 @@ import {
   Author,
   Comments,
   CommentsForm,
+  TContent,
+  TAuthor,
+  TUrl,
+  TCategory,
 } from "../../components";
-import { TAuthor, TUrl, TCategory, TContent } from "../../components";
 
 type Props = {
   post: {
@@ -25,6 +28,11 @@ type Props = {
 };
 
 const PostDetails = ({ post }: Props) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loader</div>;
+  }
+
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
