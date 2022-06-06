7add0d8a2eb47f379b2da6a1e517c67ad66ad969
André Koga <social@koga.one>
2022-06-06 16:30:55 -0300

    removed router thingy

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { getPosts, getPostDetails } from "../../services";
 import {
   PostDetail,
@@ -25,11 +24,6 @@ type Props = {
 };
 
 const PostDetails = ({ post }: Props) => {
-  const router = useRouter();
-  if (router.isFallback) {
-    return <div>Loader</div>;
-  }
-
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
