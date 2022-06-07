69960c68cd6aec9102da895b11415d100f4daec4
André Koga <social@koga.one>
2022-06-06 22:36:22 -0300

    changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,8 @@ const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>koga.one</title>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
