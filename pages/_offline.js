df29193519ee50977330cd84a50b54bc80c3c02a
André Koga <social@koga.one>
2022-06-08 16:48:44 -0300

    offline page

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,7 +1,7 @@
 const Offline = () => {
   return (
-    <div className="flex place-content-center items-center">
-      <h1 className="font-garamond">hey there, you are offline!</h1>
+    <div className="flex min-h-screen place-content-center items-center">
+      <h1 className="font-garamond text-xl">hey there, you are offline!</h1>
     </div>
   );
 };
