62d10112855f50c22b68490fb4c1dcebeaaa4be6
André Koga <social@koga.one>
2022-06-20 13:06:39 -0300

    rounded lg

--- components/PostCards/DefaultCard.tsx
+++ components/PostCards/DefaultCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const DefaultCard = ({ post }: TCardProps) => {
   return (
     <div className="h-full">
-      <div className="relative w-full overflow-hidden rounded-xl lg:min-h-[80px]">
+      <div className="relative w-full overflow-hidden rounded-lg lg:min-h-[80px]">
         <Image
           className="card-image"
           src={post.featuredImage.url}
